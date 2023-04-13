2c23a994df9c1999defcb001edee52fe5eff6d16
John Doe <johndoe@example.com>
2023-04-13 21:00:10 +0200

    finished

--- index.js
+++ index.js
@@ -1,17 +1,53 @@
 class SortedList {
-  constructor() {}
+  constructor() {
+    this.items = [];
+    this.length = this.items.length;
+  }
+  add(item) {
+    let i = 0;
+    while (i < this.items.length && this.items[i] < item) {
+      i++;
+    }
+    this.items.splice(i, 0, item);
+    this.length = this.items.length;
+  }
+  get(pos) {
+    if (pos >= this.items.length || pos < 0) {
+      throw new Error('OutOfBounds');
+    }
+    return this.items[pos];
+  }
+  max() {
+    if (this.items.length === 0) {
+      throw new Error('EmptySortedList');
+    }
+    return this.items[this.items.length - 1];
+  }
 
-  add(item) {}
+  min() {
+    if (this.items.length === 0) {
+      throw new Error('EmptySortedList');
+    }
+    return this.items[0];
+  }
+  sum() {
+    let total = 0;
+    for (let i = 0; i < this.items.length; i++) {
+      total += this.items[i];
+    }
+    return total;
+  }
 
-  get(pos) {}
-
-  max() {}
-
-  min() {}
-
-  sum() {}
-
-  avg() {}
+  avg() {
+    if (this.items.length === 0) {
+      throw new Error('EmptySortedList');
+    }
+    let total = this.sum();
+    let average = total / this.items.length;
+    return average;
+  }
 }
 
 module.exports = SortedList;
+
+
